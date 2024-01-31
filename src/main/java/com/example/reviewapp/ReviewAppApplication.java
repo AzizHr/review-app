6f735096d26293b2b2c5b386eadce9d91e0b9d2f
azizHr <azizharkati20@gmail.com>
2024-01-31 08:28:29 +0100

    first-commit

---
+++ src/main/java/com/example/reviewapp/ReviewAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.reviewapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewAppApplication.class, args);
+    }
+
+}
